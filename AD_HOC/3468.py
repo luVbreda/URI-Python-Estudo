a7aea4be1829fe1ac4dafccc004b5dfc58a6a934
luVbreda <mascarenhas-lblm@hotmail.com>
2023-12-01 14:46:49 -0300

    Update 1/12/2023

---
+++ AD_HOC/3468.py
@@ -0,0 +1,6 @@
+palavra = input()
+
+if palavra.upper() == "INTENSIDADE" or palavra.upper() == "QUANTIDADE":
+    print("mais")
+if palavra.upper() == "OPOSICAO" or palavra.upper() == "CONTRARIEDADE":
+    print("mas")
---
+++ Iniciante/3224.py
@@ -0,0 +1,7 @@
+jm = input()
+doutor = input()
+
+if(jm>doutor):
+    print("no")
+else:
+    print("go")
